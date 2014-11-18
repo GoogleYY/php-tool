aa1874e1ff3774bf4b5265322ff5359b51d12de9
770977802@qq.com <770977802@qq.com>
2014-11-18 16:25:03 +0800

    ocrking

--- rrd_helper.user.js
+++ rrd_helper.user.js
@@ -11,7 +11,7 @@
 // @require		http://www.renrendai.com/static/js/lib/jquery/1.9.1/jquery.js
 // @icon		http://www.renrendai.com/favicon.ico
 // @run-at		document-idle
-// @version 		1012
+// @version 		1013
 // @updateURL		https://raw.githubusercontent.com/wedv/php-tool/master/rrd_helper.user.js
 // @supportURL		https://github.com/wedv/php-tool
 // @homepage		https://github.com/wedv/php-tool
@@ -258,7 +258,7 @@ function lxb($, window, $debug) {
                 $dom += '<div id="lxb-title" style="width:958px;height:30px;border-bottom:1px solid red;"><div id="s_time" style="color:red;"></div>';
                 $dom += '<div style="border:1px solid gray;line-height:28px;margin-left:40%;position: absolute;top:0;"><a href="http://liuxos3.duapp.com/wx/chart" target="_blank">债权交易走势图</a></div>';
                 $dom += '<div style="border:1px solid gray;line-height:28px;margin-left:60%;position: absolute;top:0;"><p>账户余额：<span id="lxb-user-money">--</span></p></div>';
-                $dom += '<div style="width:50px;over-flow:hidden;border:1px solid gray;line-height:28px;margin-left:20%;position: absolute;top:0;"><button id="lxb-jsession-button">jsession</button><script>$("#lxb-jsession-button").click(function(){$("#lxb-buy-sessionid").toggle();});</script><input style="display:none;" type="text" id="lxb-buy-sessionid" value="" /></div>';
+                $dom += '<div style="width:58px;over-flow:hidden;border:1px solid gray;line-height:28px;margin-left:20%;position: absolute;top:0;"><button id="lxb-jsession-button">jsession</button><input style="display:none;" type="text" id="lxb-buy-sessionid" value="" /><script>$("#lxb-jsession-button").click(function(){$("#lxb-buy-sessionid").show().focus();});$("#lxb-buy-sessionid").blur(function(){$("#lxb-buy-sessionid").hide();});</script></div>';
                 $dom += '<div style="border:1px solid gray;line-height:28px;position: absolute;right:140px;top:0;"><input type="checkbox" id="lxb-open-notify">开启桌面通知</div>';
                 $dom += '<div id="lxb-showCon" style="border-left:1px solid red;position: absolute;right:0;top:0;width:120px;height:30px;float:right;padding:0 5px;color:red;font-size:22px;cursor:pointer;">O</div>';
                 $dom += '</div>';
