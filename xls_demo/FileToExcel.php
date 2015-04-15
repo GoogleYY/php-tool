0dbab56312785f79fce740635d4f9954e2f35ab7
wedv <770977802@qq.com>
2015-04-15 19:28:45 +0800

    Update FileToExcel.php

--- xls_demo/FileToExcel.php
+++ xls_demo/FileToExcel.php
@@ -19,17 +19,19 @@ class FileToXls {
 			$sheet = $phpExcel->createSheet($n);
 			$phpExcel->setActiveSheetIndex($n);
 			$phpExcel->getActiveSheet()->setTitle($i);
-			$f = file($file);
-			foreach ($f as $line => $content) {
+			$fileStream = fopen($file, 'r');
+			$line = 1;
+			while ($content = fgets($fileStream)) {
 				$items = explode('|', $content);
 				foreach ($items as $k => $item) {
-					$cell = ($cells[$k]) . ($line + 1);
+					$cell = ($cells[$k]) . $line;
 					$phpExcel->getActiveSheet()->setCellValue($cell, $item);
 				}
+				$line++;
 			}
-			unset($f);
 			$n++;
 		}
+		$phpExcel->setActiveSheetIndex(0);
 		$write = new PHPExcel_Writer_Excel2007($phpExcel);
 		$fname = T_DATE.'_report.xlsx';
 		$write->save($fname);
