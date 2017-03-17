b5a1c220d32d2ce33fd803ad553bff44637763df
wedv <770977802@qq.com>
2017-03-17 15:00:33 +0800

    Update Common.php
    增加一个可逆加密

--- Common.php
+++ Common.php
@@ -244,4 +244,31 @@ class Common {
 		}
 		return $str;
 	}
+
+    /**
+     * encrypt 
+     * 加密 
+     */
+    public static function encrypt($str, $key = 'p31&w@UY(nq}mv') {
+
+        $block = mcrypt_get_block_size('des', 'ecb');
+        $pad = $block - (strlen($str) % $block);
+        $str .= str_repeat(chr($pad), $pad);
+
+        return base64_encode(@mcrypt_encrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB));
+    }
+
+    /**
+     * decrypt
+     * 解密
+     */
+    public static function decrypt($str, $key = 'p31&w@UY(nq}mv') {
+
+        $str = base64_decode($str);
+        $str = @mcrypt_decrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB);
+        $block = mcrypt_get_block_size('des', 'ecb');
+        $pad = ord($str[($len = strlen($str)) - 1]);
+
+        return substr($str, 0, strlen($str) - $pad);
+    }
 }
